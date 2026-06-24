fa2b64fbbda3e36895cc696a3b12e7e522562372
Abbas Gholami <abbas.gholami71@gmail.com>
2026-06-24 17:32:54 +0200

    specialized schema for the zmes file format

--- src/nomad_measurements_dls/schema_packages/__init__.py
+++ src/nomad_measurements_dls/schema_packages/__init__.py
@@ -1,17 +1,14 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
+class DLSSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     def load(self):
         from nomad_measurements_dls.schema_packages.schema_package import m_package
 
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+schema_package_entry_point = DLSSchemaPackageEntryPoint(
+    name='DLSSchemaPackage',
+    description='Schema package for Dynamic Light Scattering (DLS) and Zeta Potential measurements.',
 )
